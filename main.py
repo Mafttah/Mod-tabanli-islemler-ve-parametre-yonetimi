59f38a6f9a4388c6992c46a7fc4266d97dfc0490
Mafttah <posarobit@gmail.com>
2026-03-11 21:55:25 +0000

    ACK fonsiyonu ve modlar yazilmaya baslandi

--- main.py
+++ main.py
@@ -11,3 +11,45 @@ MAX_BUFFER = 10
 data_buffer = ["", "", "", "", "", "", "", "", "", ""]
 buffer_count = 0
 
+def on_forever():
+    # ---- ACK FONKSIYONU ----
+    def send_ack(cmd_name, status):
+        ack = "A:" + str(satellite_id) + ":" + cmd_name + ":" + status
+        radio.send_string(ack)
+
+    # ---- SLEEP MODU ----
+    def mode_sleep():
+        basic.clear_screen()   # Ekrani temizle, LED'leri kapate
+
+    # ---- STANDBY MODU ----
+    def mode_standby():
+        # Sicaklik ve isik seviyesini oku
+        temp = input.temperature()
+        light_level = input.light_level()
+
+        # Verileri serial monitore yazdir
+        serial.write_line("STANDBY | Temp: " + str(temp) + "C | Light: " + str(light_level))
+
+        # Ekranda ikon goster
+        basic.show_icon(IconNames.DIAMOND)
+
+    # ---- COLLECT MODU ----
+    def mode_collect():
+        global buffer_count
+
+        # Sicaklik ve isik seviyesini oku
+        temp = input.temperature()
+        light_level = input.light_level()
+
+        # Veriyi "sicaklik-isik" string formatinda olustur
+        sample = str(temp) + ":" + str(light_level)
+
+        if buffer_count < MAX_BUFFER:
+            data_buffer[buffer_count] = sample
+            buffer_count += 1
+            serial.write_line("COLLECT | Buffer[" + str(buffer_count) + "/" + str(MAX_BUFFER) + "]: " + sample)
+
+        
+
+
+
--- main.ts
+++ main.ts
@@ -9,3 +9,45 @@ let light_threshold = 100
 let MAX_BUFFER = 10
 let data_buffer = ["", "", "", "", "", "", "", "", "", ""]
 let buffer_count = 0
+function on_forever() {
+    //  ---- ACK FONKSIYONU ----
+    function send_ack(cmd_name: any, status: any) {
+        let ack = "A:" + ("" + satellite_id) + ":" + cmd_name + ":" + status
+        radio.sendString(ack)
+    }
+    
+    //  ---- SLEEP MODU ----
+    function mode_sleep() {
+        basic.clearScreen()
+    }
+    
+    //  Ekrani temizle, LED'leri kapate
+    //  ---- STANDBY MODU ----
+    function mode_standby() {
+        //  Sicaklik ve isik seviyesini oku
+        let temp = input.temperature()
+        let light_level = input.lightLevel()
+        //  Verileri serial monitore yazdir
+        serial.writeLine("STANDBY | Temp: " + ("" + temp) + "C | Light: " + ("" + light_level))
+        //  Ekranda ikon goster
+        basic.showIcon(IconNames.Diamond)
+    }
+    
+    //  ---- COLLECT MODU ----
+    function mode_collect() {
+        
+        //  Sicaklik ve isik seviyesini oku
+        let temp = input.temperature()
+        let light_level = input.lightLevel()
+        //  Veriyi "sicaklik-isik" string formatinda olustur
+        let sample = "" + temp + ":" + ("" + light_level)
+        if (buffer_count < MAX_BUFFER) {
+            data_buffer[buffer_count] = sample
+            buffer_count += 1
+            serial.writeLine("COLLECT | Buffer[" + ("" + buffer_count) + "/" + ("" + MAX_BUFFER) + "]: " + sample)
+        }
+        
+    }
+    
+}
+
